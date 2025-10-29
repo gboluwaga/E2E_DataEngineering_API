62190e106039d731da7fd9467e749300344c3484
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-10-29 20:33:36 +0100

    saved the video data in data folder path using context function

--- video_statistics.py
+++ video_statistics.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import date
 import json
 import os
 from dotenv import load_dotenv
+
 
 load_dotenv(dotenv_path="./.env")
 
@@ -99,8 +101,16 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_as_json_to_file_path(extracted_data):
+    file_path = f"./data/youtube_data_{date.today()}.json"
+
+    with open(file_path,"w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
     playlist_id = get_playlistid()
     video_ids = get_video_id(playlist_id)
-    extract_video_data(video_ids)
+    video_data = extract_video_data(video_ids)
+    save_as_json_to_file_path(video_data)
 
