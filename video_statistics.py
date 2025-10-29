1e89cb30c3e078ea04c351c996c97bd09a7f6d05
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-10-29 17:41:17 +0100

    added the video id of the playlist with page token

--- video_statistics.py
+++ video_statistics.py
@@ -7,6 +7,8 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResult = 50
+
 
 def get_playlistid ():
     try :
@@ -22,6 +24,40 @@ def get_playlistid ():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_id(playlist_id):
+
+    video_ids = []
+
+    page_token = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResult}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+        while True:
+
+            url = base_url
+
+            if page_token:
+                url += f"&page_token={page_token}"
+
+            response = requests.get(url, verify= False)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlistid()
+    playlist_id = get_playlistid()
+    get_video_id(playlist_id)
 
