0b83bbdc8c08e12aad3e5b1aa7c639100ea4f854
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-11-04 10:22:35 +0100

    added deleted and updated operation

--- dags/datawarehouse/data_modification.py
+++ dags/datawarehouse/data_modification.py
@@ -40,4 +40,63 @@ def insert_rows(cur, conn, schema, row):
         logger.error(f"Error inserting row with Video_ID: {row[video_id]}")
         raise e
 
+def update_rows(cur, conn, schema, row):
 
+    try:
+        # staging
+        if schema == "staging":
+            video_id = "video_id"
+            upload_date = "published_date"
+            video_title = "title"
+            video_views = "viewcount"
+            likes_count = "likeCount"
+            comments_count = "commentcount"
+        # core
+        else:
+            video_id = "Video_ID"
+            upload_date = "Upload_Date"
+            video_title = "Video_Title"
+            video_views = "Video_Views"
+            likes_count = "Likes_Count"
+            comments_count = "Comments_Count"
+
+        cur.execute(
+            f"""
+            UPDATE {schema}.{table}
+            SET "Video_Title" = %({video_title})s,
+                "Video_Views" = %({video_views})s, 
+                "Likes_Count" = %({likes_count})s, 
+                "Comments_Count" = %({comments_count})s
+            WHERE "Video_ID" = %({video_id})s AND "Upload_Date" = %({upload_date})s;
+            """,
+            row,
+        )
+
+        conn.commit()
+
+        logger.info(f"Updated row with Video_ID: {row[video_id]}")
+
+    except Exception as e:
+        logger.error(f"Error updating row with Video_ID: {row[video_id]} - {e}")
+        raise e
+
+
+def delete_rows(cur, conn, schema, ids_to_delete):
+
+    try:
+
+        ids_to_delete = f"""({', '.join(f"'{id}'" for id in ids_to_delete)})"""
+
+        cur.execute(
+            f"""
+            DELETE FROM {schema}.{table}
+            WHERE "Video_ID" IN {ids_to_delete};
+            """
+        )
+
+        conn.commit()
+        logger.info(f"Deleted rows with Video_IDs: {ids_to_delete}")
+
+    except Exception as e:
+        logger.error(f"Error deleting rows with Video_IDs: {ids_to_delete} - {e}")
+        raise e
