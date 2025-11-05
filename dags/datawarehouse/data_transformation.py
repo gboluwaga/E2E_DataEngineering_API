08699d46f6571fc80a7706d612acab86a1c63757
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-11-05 07:10:34 +0100

    modified the duration column and added a video type column to the transformation

--- dags/datawarehouse/data_transformation.py
+++ dags/datawarehouse/data_transformation.py
@@ -1,7 +1,30 @@
+from datetime import timedelta, datetime
+
 def parse_duration(duration_str):
-    """Parse ISO 8601 duration string to total seconds."""
-    import isodate
-    duration_str = duration_str.replace("P","").replace("T","")
-    
-    isodate.parse_duration(duration_str)
-    return int(duration.total_seconds())
+
+    duration_str = duration_str.replace("P", "").replace("T", "")
+
+    components = ["D", "H", "M", "S"]
+    values = {"D": 0, "H": 0, "M": 0, "S": 0}
+
+    for component in components:
+        if component in duration_str:
+            value, duration_str = duration_str.split(component)
+            values[component] = int(value)
+
+    total_duration = timedelta(
+        days=values["D"], hours=values["H"], minutes=values["M"], seconds=values["S"]
+    )
+
+    return total_duration
+
+
+def transform_data(row):
+
+    duration_td = parse_duration(row["Duration"])
+
+    row["Duration"] = (datetime.min + duration_td).time()
+
+    row["Video_Type"] = "Shorts" if duration_td.total_seconds() <= 60 else "Normal"
+
+    return row
