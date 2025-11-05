5f5e577897cf988decb82aa786c507c36a9a5487
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-11-05 07:31:08 +0100

    added task to the DAG for airflow

--- dags/datawarehouse/dwh.py
+++ dags/datawarehouse/dwh.py
@@ -2,3 +2,112 @@ from datawarehouse.data_utils import get_con_cursor,close_con_cursor,create_sche
 from datawarehouse.data_loading import load_path
 from datawarehouse.data_transformation import transform_data
 from datawarehouse.data_modification import insert_rows, update_rows, delete_rows
+
+
+import logging
+
+from airflow.decorators import dag, task
+
+logger = logging.getLogger(__name__)
+table = "youtube_video_statistics"
+
+@task
+def staging_table():
+
+    schema = "staging"
+
+    conn, cur = None, None
+
+    try:
+
+        conn, cur = get_con_cursor()
+
+        YT_data = load_path()
+
+        create_schema(schema)
+        create_table(schema)
+
+        table_ids = get_video_ids(cur, schema)
+
+        for row in YT_data:
+
+            if len(table_ids) == 0:
+                insert_rows(cur, conn, schema, row)
+
+            else:
+                if row["video_id"] in table_ids:
+                    update_rows(cur, conn, schema, row)
+                else:
+                    insert_rows(cur, conn, schema, row)
+
+        ids_in_json = {row["video_id"] for row in YT_data}
+
+        ids_to_delete = set(table_ids) - ids_in_json
+
+        if ids_to_delete:
+            delete_rows(cur, conn, schema, ids_to_delete)
+
+        logger.info(f"{schema} table update completed")
+
+    except Exception as e:
+        logger.error(f"An error occurred during the update of {schema} table: {e}")
+        raise e
+
+    finally:
+        if conn and cur:
+            close_con_cursor(conn, cur)
+
+
+@task
+def core_table():
+
+    schema = "core"
+
+    conn, cur = None, None
+
+    try:
+        conn, cur = get_con_cursor()
+
+        create_schema(schema)
+        create_table(schema)
+
+        table_ids = get_video_ids(cur, schema)
+
+        current_video_ids = set()
+
+        cur.execute(f"SELECT * FROM staging.{table};")
+        rows = cur.fetchall()
+
+        for row in rows:
+
+            current_video_ids.add(row["Video_ID"])
+
+            if len(table_ids) == 0:
+                transformed_row = transform_data(row)
+                insert_rows(cur, conn, schema, transformed_row)
+
+            else:
+                transformed_row = transform_data(row)
+
+                if transformed_row["Video_ID"] in table_ids:
+                    update_rows(cur, conn, schema, transformed_row)
+
+                else:
+                    insert_rows(cur, conn, schema, transformed_row)
+
+        ids_to_delete = set(table_ids) - current_video_ids
+
+        if ids_to_delete:
+            delete_rows(cur, conn, schema, ids_to_delete)
+
+        logger.info(f"{schema} table update completed")
+
+    except Exception as e:
+        # Log any exceptions that occur
+        logger.error(f"An error occurred during the update of {schema} table: {e}")
+        raise e
+
+    finally:
+        # Ensure the connection and cursor are closed
+        if conn and cur:
+            close_con_cursor(conn, cur)
