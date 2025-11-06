2d089ec03ca365e47aa7d9a3eb992d7321b956c2
Gboluwaga Akinleye <gboluwaga.akinleye@spironet.com>
2025-11-06 09:42:54 +0100

    A new dag for insert

--- dags/main.py
+++ dags/main.py
@@ -36,3 +36,21 @@ with DAG(
 
     #define task dependencies
     playlist_id >> video_ids >> extracted_data >> save_to_json
+
+
+with DAG(
+    dag_id='update_db',
+    default_args=default_args,
+    description='Insert and Update',
+    schedule_interval='0 * * * *',
+    catchup=False,
+) as dag:
+
+    #Define Task
+    playlist_id = get_playlistid()
+    video_ids = get_video_id(playlist_id)
+    extracted_data = extract_video_data(video_ids)
+    save_to_json = save_as_json_to_file_path(extracted_data)
+
+    #define task dependencies
+    playlist_id >> video_ids >> extracted_data >> save_to_json
