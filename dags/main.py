d98bf77289533e7125201269791e947fa912ff50
gboluwaga <gboluwagaakinleye202@gmail.com>
2025-11-25 00:30:17 +0100

    Added a new structure to auto trigger dag without doing so Manually

--- dags/main.py
+++ dags/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from api.video_statistics import get_playlistid, get_video_id, extract_video_data, save_as_json_to_file_path
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 local_tz = pendulum.timezone("Africa/Lagos")
 
@@ -25,13 +26,16 @@ default_args = {
 staging_schema ="staging"
 core_schema = "core"
 
+#1 dag produce youtube video statistics and save as json
+
+
 with DAG(
     dag_id='youtube_video_statistics_dag',
     default_args=default_args,
     description='A DAG to extract YouTube video statistics and save as JSON',
     schedule_interval='0 * * * *',
     catchup=False,
-) as dag:
+) as dag_produce:
 
     #Define Task
     playlist_id = get_playlistid()
@@ -39,32 +43,46 @@ with DAG(
     extracted_data = extract_video_data(video_ids)
     save_to_json = save_as_json_to_file_path(extracted_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
     #define task dependencies
-    playlist_id >> video_ids >> extracted_data >> save_to_json
+    playlist_id >> video_ids >> extracted_data >> save_to_json >> trigger_update_db
 
 
+
+#2 dag update staging and core table
 with DAG(
     dag_id='update_db',
     default_args=default_args,
     description='Insert and Update and staging and core table',
     schedule_interval='0 */2 * * *',
     catchup=False,
-) as dag:
+    schedule = None,
+) as dag_update:
 
     #Define Task
     staging_tables = staging_table()
     core_tables = core_table()
 
+    trigger_data_quality  = TriggerDagRunOperator(
+        task_id="trigger_data_quality ",
+        trigger_dag_id="data_quality_checks",
+    )
+
     #define task dependencies
-    staging_tables >> core_tables
+    staging_tables >> core_tables >> trigger_data_quality   
 
-
+#3 dag data quality check using soda
 with DAG(
     dag_id='data_quality_checks',
     default_args=default_args,
     description='Run data quality checks using Soda',
     schedule_interval='0 */4 * * *',
     catchup=False,
+    schedule = None,
 ) as dag:
 
     #Define Task
